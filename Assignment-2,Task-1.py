41696a71ae9e2b442432e35b0f13977aa7b175cd
RitikGoyal112 <RRITIKGGOYALJJI@GMAIL.COM>
2025-07-11 23:58:17 +0530

    Add files via upload

---
+++ Assignment-2,Task-1.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number: "))
+
+if num%2==0:
+    result="even"
+else:
+    result="odd"
+
+print(str(num) +" is an " + result + " number.")
---
+++ Assignment-3,Task-2.py
@@ -0,0 +1,12 @@
+import math
+
+number = float(input("Enter a number: "))
+
+sqrt = math.sqrt(number)
+log = math.log(number)
+sine = math.sin(number)
+
+print("Square root: ",sqrt)
+print("Logarithm: ",log)
+print("Sine: ",sine)
+
