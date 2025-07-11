c595e782a03f28d0fee6a5313dde87067979778e
RitikGoyal112 <RRITIKGGOYALJJI@GMAIL.COM>
2025-07-11 23:53:56 +0530

    Add files via upload

---
+++ Assignment-1,Task-1.py
@@ -0,0 +1,22 @@
+
+num_1 = float(input("Enter the first number: "))
+num_2 = float(input("Enter the first number: "))
+
+addition       = num_1 + num_2
+subtraction    = num_1 - num_2
+multiplication = num_1 * num_2
+
+if num_2 != 0:
+    division = num_1 / num_2
+else:
+    division = "Undefined (division by zero)"
+
+print("\nAddition: ",addition)
+print("Subtraction: ",subtraction)
+print("Multiplication: ",multiplication)
+print("Division: ",division)
+
+
+
+
+
---
+++ Assignment-1,Task-2.py
@@ -0,0 +1,4 @@
+name = input("Enter your first name: ")
+surname = input("Enter your last name: ")
+full_name = name+" "+surname
+print("\nHello, "+full_name+"! Welcome to the Python program.")
